cc015a7cad4c11c3dffa8e610c7c4f79e2dabcc2
saccec <saccec@gmail.com>
2026-07-27 20:22:30 -0500

    Feat: Actualizacion funcion servidor

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ app.get('/', (req, res) => {
   res.send('¡Hola! El servidor web en Node.js está funcionando correctamente.');
 });
 
+app.get('/buscar', (req, res) => {
+  
+  res.send('¡Hola! El servidor web en Node.js está buscando correctamente.');
+});
+
 // Iniciar el servidor
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
